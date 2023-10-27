5aa6032d52251c25a9c85468ada7583be5336673
Harsh Bangari <harshrock2010@gmail.com>
2023-10-27 16:18:44 +0530

    initial commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+import figlet from "figlet";
+
+const server = Bun.serve({
+    port: 3000,
+    fetch(req) {
+        const body = figlet.textSync("Hi Bun!");
+        return new Response(body);
+    },
+  });
+  
+  console.log(`Listening on http://localhost:${server.port} ...`);
+  
---
+++ sample-jsx.tsx
@@ -0,0 +1,1 @@
+console.log(<div>This is sample div</div>)
